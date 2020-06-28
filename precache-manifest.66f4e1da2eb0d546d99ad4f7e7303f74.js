a5d380aa638dad53b0bd0384073f6807d1d53827
Jeancarlosayala <jeancarlosayala57@gmail.com>
2020-06-28 17:36:35 -0500

    Updates

---
+++ precache-manifest.66f4e1da2eb0d546d99ad4f7e7303f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea695f3ffd2f385378df53572ef891a1",
+    "url": "/searchMonsters/index.html"
+  },
+  {
+    "revision": "c5fd6172ad8d8a949905",
+    "url": "/searchMonsters/static/css/main.4f72913d.chunk.css"
+  },
+  {
+    "revision": "d702525802753da5ecd7",
+    "url": "/searchMonsters/static/js/2.5e077a7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchMonsters/static/js/2.5e077a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fd6172ad8d8a949905",
+    "url": "/searchMonsters/static/js/main.16dba355.chunk.js"
+  },
+  {
+    "revision": "a99fa52eee31071ff845",
+    "url": "/searchMonsters/static/js/runtime-main.894cd785.js"
+  }
+]);
